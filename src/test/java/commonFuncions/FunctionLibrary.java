3863997bc7bb638e3c62a69630f29ae24a8e889f
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-03-08 18:23:28 +0530

    Modified classes

--- src/test/java/commonFuncions/FunctionLibrary.java
+++ src/test/java/commonFuncions/FunctionLibrary.java
@@ -1,10 +1,15 @@
 package commonFuncions;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -17,98 +22,160 @@ import utilities.PropertyFileUtil;
  * Module Name: All Modules
  */
 public class FunctionLibrary {
-public static WebDriver driver;
-//method for start Browser
-public static WebDriver startBrowser()throws Throwable
-{
-	if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("chrome"))
+	public static WebDriver driver;
+	public static String ExpectedData ="";
+	public static String ActualData="";
+	//method for start Browser
+	public static WebDriver startBrowser()throws Throwable
 	{
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
+		if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("firefox"))
+		{
+			driver = new FirefoxDriver();
+		}
+		else
+		{
+			Reporter.log("Browser value is Not Matching",true);
+		}
+		return driver;
 	}
-	else if(PropertyFileUtil.getValueForKey("Browser").equalsIgnoreCase("firefox"))
+	//method for open application
+	public static void applicationUrl(WebDriver driver)throws Throwable
 	{
-		driver = new FirefoxDriver();
+		driver.get(PropertyFileUtil.getValueForKey("Url"));
 	}
-	else
+	//method for wait for element
+	public static void waitForElement(WebDriver driver,String LocatorType,String LocatorValue,String MyWait)
 	{
-		Reporter.log("Browser value is Not Matching",true);
+		WebDriverWait myWait =new WebDriverWait(driver, Integer.parseInt(MyWait));
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		}
+		else if(LocatorType.equalsIgnoreCase("name"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.name(LocatorValue)));
+		}
+		else if(LocatorType.equalsIgnoreCase("id"))
+		{
+			myWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(LocatorValue)));
+		}
 	}
-	return driver;
-}
-//method for open application
-public static void applicationUrl(WebDriver driver)throws Throwable
-{
-	driver.get(PropertyFileUtil.getValueForKey("Url"));
-}
-//method for wait for element
-public static void waitForElement(WebDriver driver,String LocatorType,String LocatorValue,String MyWait)
-{
-	WebDriverWait myWait =new WebDriverWait(driver, Integer.parseInt(MyWait));
-	if(LocatorType.equalsIgnoreCase("xpath"))
+	//method for type action
+	public static void typeAction(WebDriver driver,String LocatorType,String LocatorValue,String TestData)
 	{
-		myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(LocatorValue)));
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.name(LocatorValue)).clear();
+			driver.findElement(By.name(LocatorValue)).sendKeys(TestData);
+		}
+		else if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).clear();
+			driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		}
+		else if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.id(LocatorValue)).clear();
+			driver.findElement(By.id(LocatorValue)).sendKeys(TestData);
+		}
 	}
-	else if(LocatorType.equalsIgnoreCase("name"))
+	//method for click action
+	public static void clickAction(WebDriver driver,String LocatorType,String LocatorValue)
 	{
-		myWait.until(ExpectedConditions.visibilityOfElementLocated(By.name(LocatorValue)));
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocatorValue)).click();
+		}
+		else if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.name(LocatorValue)).click();
+		}
+		else if(LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.id(LocatorValue)).sendKeys(Keys.ENTER);
+		}
 	}
-	else if(LocatorType.equalsIgnoreCase("id"))
+	//validate page title
+	public static void validateTitle(WebDriver driver,String ExpectedTitle)
 	{
-		myWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(LocatorValue)));
+		String Actualttitle = driver.getTitle();
+		try {
+			Assert.assertEquals(Actualttitle, ExpectedTitle,"Title is Not Matching");
+		}catch(Throwable t)
+		{
+			System.out.println(t.getMessage());
+		}
 	}
-}
-//method for type action
-public static void typeAction(WebDriver driver,String LocatorType,String LocatorValue,String TestData)
-{
-	if(LocatorType.equalsIgnoreCase("name"))
+	//method for close browser
+	public static void closeBrowser(WebDriver driver)
 	{
-		driver.findElement(By.name(LocatorValue)).clear();
-		driver.findElement(By.name(LocatorValue)).sendKeys(TestData);
+		driver.quit();
 	}
-	else if(LocatorType.equalsIgnoreCase("xpath"))
+	//method for mouse click
+	public static void mouseClick(WebDriver driver) throws Throwable
 	{
-		driver.findElement(By.xpath(LocatorValue)).clear();
-		driver.findElement(By.xpath(LocatorValue)).sendKeys(TestData);
+		Actions ac = new Actions(driver);
+		ac.moveToElement(driver.findElement(By.xpath("//a[.='Stock Items ']"))).perform();
+		Thread.sleep(3000);
+		ac.moveToElement(driver.findElement(By.xpath("(//a[.='Stock Categories'])[2]"))).click().perform();
 	}
-	else if(LocatorType.equalsIgnoreCase("id"))
+	//method for stock table
+	public static void stockTable(WebDriver driver,String Expected_Data)throws Throwable
 	{
-		driver.findElement(By.id(LocatorValue)).clear();
-		driver.findElement(By.id(LocatorValue)).sendKeys(TestData);
+		//click search panel if search textbox not displayed 
+		if(!driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-textbox"))).isDisplayed())
+			//if search textbot not displayed click search panel
+			driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-panel"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-textbox"))).sendKeys(Expected_Data);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-button"))).click();
+		Thread.sleep(3000);
+		//capture category name from tabale
+		String Actaul_Data =driver.findElement(By.xpath("//table[@id='tbl_a_stock_categorieslist']/tbody/tr[1]/td[4]/div/span/span")).getText();
+		System.out.println(Expected_Data+"    "+Actaul_Data);
+		Assert.assertEquals(Expected_Data, Actaul_Data, "Categery Name Not Found in Table");
 	}
-}
-//method for click action
-public static void clickAction(WebDriver driver,String LocatorType,String LocatorValue)
-{
-	if(LocatorType.equalsIgnoreCase("xpath"))
+	//method for capture data
+	public static void captureData(WebDriver driver,String LocatorType,String Locatorvalue)
 	{
-		driver.findElement(By.xpath(LocatorValue)).click();
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			ExpectedData = driver.findElement(By.xpath(Locatorvalue)).getAttribute("value");
+		}
+		else
+		{
+			System.out.println("Unable to capture value");
+		}
 	}
-	else if(LocatorType.equalsIgnoreCase("name"))
+	//method for supplier table
+	public static void supplierTable(WebDriver driver)throws Throwable
 	{
-		driver.findElement(By.name(LocatorValue)).click();
+		//click search panel if search textbox not displayed 
+		if(!driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-textbox"))).isDisplayed())
+			//if search textbot not displayed click search panel
+			driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-panel"))).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-textbox"))).sendKeys(ExpectedData);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath(PropertyFileUtil.getValueForKey("search-button"))).click();
+		Thread.sleep(3000);
+		ActualData= driver.findElement(By.xpath("//table[@id='tbl_a_supplierslist']/tbody/tr[1]/td[6]/div/span/span")).getText();
+		Reporter.log(ExpectedData+"     "+ActualData,true);
+		Assert.assertEquals(ExpectedData, ActualData,"Supplier number is Not Matching");
 	}
-	else if(LocatorType.equalsIgnoreCase("id"))
+	//java time stamp
+	public static String generateDate()
 	{
-		driver.findElement(By.id(LocatorValue)).sendKeys(Keys.ENTER);
+		Date date = new Date();
+		DateFormat df = new SimpleDateFormat("YYYY_MM_dd hh_mm_ss");
+		return df.format(date);
 	}
-}
-//validate page title
-public static void validateTitle(WebDriver driver,String ExpectedTitle)
-{
-	String Actualttitle = driver.getTitle();
-	try {
-	Assert.assertEquals(Actualttitle, ExpectedTitle,"Title is Not Matching");
-	}catch(Throwable t)
-	{
-		System.out.println(t.getMessage());
-	}
-}
-//method for close browser
-public static void closeBrowser(WebDriver driver)
-{
-	driver.quit();
-}
 }
 
 
--- src/test/java/driverFactory/DriverScript.java
+++ src/test/java/driverFactory/DriverScript.java
@@ -2,11 +2,17 @@ package driverFactory;
 
 import org.openqa.selenium.WebDriver;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import commonFuncions.FunctionLibrary;
 import utilities.ExcelFileUtil;
 
 public class DriverScript {
 public static WebDriver driver;
+ExtentReports reports;
+ExtentTest logger;
 String Inputpath ="D:\\OJTLiveProjet\\ERP_HybridFrameWork\\FileInput\\DataEngine.xlsx";
 String Outputpath ="D:\\OJTLiveProjet\\ERP_HybridFrameWork\\FileOutput\\HybridResults.xlsx";
 public void starTest()throws Throwable
@@ -22,9 +28,11 @@ public void starTest()throws Throwable
 		if(Execution_Status.equalsIgnoreCase("Y"))
 		{
 			String TCModule =xl.getCellData("MasterTestCases", i, 1);
+			reports = new ExtentReports("./ExtentReports/"+TCModule+"_"+FunctionLibrary.generateDate()+".html");
 			//iterate all rows in TCModule
 			for(int j=1;j<=xl.rowCount(TCModule);j++)
 			{
+				logger = reports.startTest(TCModule);
 			String Description =xl.getCellData(TCModule, j, 0);
 			String Object_Type = xl.getCellData(TCModule, j, 1);
 			String Locator_Type = xl.getCellData(TCModule, j, 2);
@@ -34,39 +42,68 @@ public void starTest()throws Throwable
 				if(Object_Type.equalsIgnoreCase("startBrowser"))
 				{
 					driver =FunctionLibrary.startBrowser();
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("applicationUrl"))
 				{
 					FunctionLibrary.applicationUrl(driver);
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("waitForElement"))
 				{
 					FunctionLibrary.waitForElement(driver, Locator_Type, Locator_Value, Test_Data);
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("typeAction"))
 				{
 					FunctionLibrary.typeAction(driver, Locator_Type, Locator_Value, Test_Data);
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("clickAction"))
 				{
 					FunctionLibrary.clickAction(driver, Locator_Type, Locator_Value);
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("validateTitle"))
 				{
 					FunctionLibrary.validateTitle(driver, Test_Data);
+					logger.log(LogStatus.INFO, Description);
 				}
 				else if(Object_Type.equalsIgnoreCase("closeBrowser"))
 				{
 					FunctionLibrary.closeBrowser(driver);
+					logger.log(LogStatus.INFO, Description);
+				}
+				else if(Object_Type.equalsIgnoreCase("mouseClick"))
+				{
+					FunctionLibrary.mouseClick(driver);
+					logger.log(LogStatus.INFO, Description);
+				}
+				else if(Object_Type.equalsIgnoreCase("stockTable"))
+				{
+					FunctionLibrary.stockTable(driver, Test_Data);
+					logger.log(LogStatus.INFO, Description);
+				}
+				else if(Object_Type.equalsIgnoreCase("captureData"))
+				{
+					FunctionLibrary.captureData(driver, Locator_Type, Locator_Value);
+					logger.log(LogStatus.INFO, Description);
+				}
+				else if(Object_Type.equalsIgnoreCase("supplierTable"))
+				{
+					FunctionLibrary.supplierTable(driver);
+					logger.log(LogStatus.INFO, Description);
 				}
 				//write as pass
 				xl.setCellData(TCModule, j, 5, "Pass", Outputpath);
+				logger.log(LogStatus.PASS, Description);
 				ModuleStatus="True";
 			}catch(Throwable t)
 			{
 				System.out.println(t.getMessage());
 				//write as fail
 				xl.setCellData(TCModule, j, 5, "Fail", Outputpath);
+				logger.log(LogStatus.FAIL, Description);
 				ModuleStatus="False";
 			}
 			if(ModuleStatus.equalsIgnoreCase("True"))
@@ -77,6 +114,8 @@ public void starTest()throws Throwable
 			{
 				xl.setCellData("MasterTestCases", i, 3, "Fail", Outputpath);
 			}
+			reports.endTest(logger);
+			reports.flush();
 			}
 		}
 		else
